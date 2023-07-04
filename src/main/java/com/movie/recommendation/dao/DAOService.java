aa9021be8078babb793d700928d5282e7fcacccc
Anshul0707 <anshul.markwade786@gmail.com>
2023-07-04 11:15:30 +0530

    updated code

--- src/main/java/com/movie/recommendation/dao/DAOService.java
+++ src/main/java/com/movie/recommendation/dao/DAOService.java
@@ -3,6 +3,7 @@ package com.movie.recommendation.dao;
 import com.movie.recommendation.dto.DatabaseConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
 import javax.sql.DataSource;
 
 @Service
@@ -13,7 +14,7 @@ public class DAOService {
     private final JdbcTemplate jdbcTemplate;
     private final DatabaseConfiguration databaseConfiguration;
 
-    public DAOService(DataSource dataSource,JdbcTemplate jdbcTemplate){
+    public DAOService(DataSource dataSource, JdbcTemplate jdbcTemplate) {
         this.dataSource = dataSource;
         this.jdbcTemplate = jdbcTemplate;
         this.databaseConfiguration = getDatabaseConfig();
